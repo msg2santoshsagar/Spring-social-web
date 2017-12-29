ba8acc29feb0c9763170d8f4bde0767dbfbbf00d
1197249 <1197249>
2017-12-29 18:20:19 +0530

    Http Interceptor updated

--- src/app/interceptor/custom-http-interceptor.ts
+++ src/app/interceptor/custom-http-interceptor.ts
@@ -1,8 +1,16 @@
+import 'rxjs/add/operator/do';
+
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { HttpResponse } from "@angular/common/http";
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class CustomHttpInterceptor implements HttpInterceptor {
+
+    constructor(private router : Router){}
 
     /**
      * Custom Http Interceptor
@@ -11,9 +19,25 @@ export class CustomHttpInterceptor implements HttpInterceptor {
      * 
      */
 
-    intercept(request : HttpRequest<any> , next : HttpHandler){
-       console.log("Custom Http Interceptor called");
-       return next.handle(request);
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log("Custom Http Interceptor called");
+        return next.handle(request).do(
+            (event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                    // TODO : If you can see response
+                   // console.log("You got response", event);
+                }
+            },
+            (err: any) => {
+                if( err instanceof HttpErrorResponse){
+                    if (err.status === 401) {
+                       //UnAuthorized Error ocucred
+                       console.error("User is unauthorized ",err);
+                      }
+                }
+            }
+        );
     }
+
 
 }
